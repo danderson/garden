35eb737fa5a554628c0723052f3fc580c517d4aa
David Anderson <dave@natulte.net>
2023-02-26 09:59:14 -0800

    Basic box index
    Signed-off-by: David Anderson <dave@natulte.net>

--- boxinventory/views.py
+++ boxinventory/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Box
 
 def index(request):
-    return HttpResponse("Hello world (box index)")
+    boxes = Box.objects.all()
+    ctx = {'boxes': boxes}
+    return render(request, 'boxinventory/index.html', ctx)
