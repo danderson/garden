5caac07749c40b613ed42d143708f3639e56131e
David Anderson <dave@natulte.net>
2023-02-26 10:09:20 -0800

    Correctly display empty box count
    Signed-off-by: David Anderson <dave@natulte.net>

--- boxinventory/views.py
+++ boxinventory/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 from .models import Box
 
 def index(request):
-    boxes = Box.objects.all()
+    boxes = Box.objects.order_by('name')
     ctx = {'boxes': boxes}
     return render(request, 'boxinventory/index.html', ctx)
