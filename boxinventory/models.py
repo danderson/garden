2d268fad7c252dd1370b0d6096e46837b5ec59c6
David Anderson <dave@natulte.net>
2023-02-26 11:37:04 -0800

    alphabetize boxes
    Signed-off-by: David Anderson <dave@natulte.net>

--- boxinventory/models.py
+++ boxinventory/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Box(models.Model):
     class Meta:
         verbose_name_plural = "boxes"
+        ordering = ['name']
 
     name = models.CharField(max_length=200)
     want_qr = models.BooleanField('QR code?', default=False)
