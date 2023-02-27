28f0ae5e916d74c21c0047c7601bd5fb0f781a09
David Anderson <dave@natulte.net>
2023-02-26 16:35:50 -0800

    herbarium: refine data model
    Signed-off-by: David Anderson <dave@natulte.net>

--- herbarium/admin.py
+++ herbarium/admin.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
 
-from .models import Family, Plant, Variety
+from .models import Family, Plant, Variety, PlantName
 
 class VarietyInline(admin.StackedInline):
     model = Variety
     extra = 1
 
+class PlantNameInline(admin.StackedInline):
+    model = PlantName
+    extra = 1
+
 class PlantAdmin(admin.ModelAdmin):
-    inlines = [VarietyInline]
+    inlines = [PlantNameInline, VarietyInline]
 
 admin.site.register(Family)
 admin.site.register(Plant, PlantAdmin)
--- herbarium/models.py
+++ herbarium/models.py
@@ -5,25 +5,59 @@ class Family(models.Model):
         verbose_name_plural = "families"
 
     name = models.CharField(max_length=200)
-    latin = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
 class Plant(models.Model):
-    name = models.CharField(max_length=200)
-    latin = models.CharField(max_length=200)
     family = models.ForeignKey(Family, on_delete=models.CASCADE)
+
+    edible = models.BooleanField('Edible?', default=False)
+    needs_trellis = models.BooleanField('Needs trellis?', default=False)
+    needs_bird_netting = models.BooleanField('Needs bird netting?', default=False)
+    is_keto = models.BooleanField('Is keto?', default=False)
+    native = models.BooleanField('Native plant?', default=False)
+    invasive = models.BooleanField('Is invasive?', default=False)
+    is_cover = models.BooleanField('Cover crop?', default=False)
+    grow_from_seed = models.BooleanField('Good to grow from seeds?', default=True)
+
+    class Type(models.TextChoices):
+        VEGETABLE = 'V', 'Vegetable'
+        FRUIT = 'F', 'Fruit'
+        HERB = 'H', 'Herb'
+        FLOWER = 'L', 'Flower'
+        GREEN = 'G', 'Green'
+    
+    class Lifespan(models.TextChoices):
+        ANNUAL = 'A', 'Annual'
+        BIENNIAL = 'B', 'Biennial'
+        PERENNIAL = 'P', 'Perennial'
+
+    type = models.CharField(max_length=1, choices=Type.choices)
+    lifespan = models.CharField(max_length=1, choices=Lifespan.choices)
+    bad_for_cats = models.BooleanField('Bad for cats?', default=False)
+    deer_resistant = models.BooleanField('Deer resistant?', default=False)
+
+    def __str__(self):
+        return "{} ({}, {} varieties)".format(self.primary_name(), self.family, self.variety_set.count())
+
+    def primary_name(self):
+        return self.plantname_set.all()[0].name
+
+class PlantName(models.Model):
+    class Meta:
+        verbose_name_plural = "plant names"
+
+    name = models.CharField(max_length=200)
+    plant = models.ForeignKey(Plant, on_delete=models.CASCADE)
 
 class Variety(models.Model):
     class Meta:
         verbose_name_plural = "varieties"
 
     name = models.CharField(max_length=200)
-    latin = models.CharField(max_length=200)
     plant = models.ForeignKey(Plant, on_delete=models.CASCADE)
+    heat_sensitive = models.BooleanField('Cover during heatwaves?', default=False)
 
-    edible = models.BooleanField(default=False)
-    needs_trellis = models.BooleanField(default=False)
-    needs_bird_netting = models.BooleanField(default=False)
-    is_keto = models.BooleanField(default=False)
-    native = models.BooleanField(default=False)
-    invasive = models.BooleanField(default=False)
-    is_cover = models.BooleanField(default=False)
+    def __str__(self):
+        return "{} {} ({})".format(self.name, self.plant.primary_name(), self.plant.family.name)
