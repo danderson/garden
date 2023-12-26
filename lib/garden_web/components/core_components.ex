ec92152e4a6ee7d0768400503ae0534cee1ec049
David Anderson <dave@natulte.net>
2023-12-25 21:56:58 -0800

    WIP: fuck around with styling, hashtag mobile first

--- lib/garden_web/components/core_components.ex
+++ lib/garden_web/components/core_components.ex
@@ -114,21 +114,23 @@ defmodule GardenWeb.CoreComponents do
       phx-click={JS.push("lv:clear-flash", value: %{key: @kind}) |> hide("##{@id}")}
       role="alert"
       class={[
-        "fixed top-2 right-2 mr-2 w-80 sm:w-96 z-50 rounded-lg p-3 ring-1",
+        "p-2 ring-1",
         @kind == :info && "bg-emerald-50 text-emerald-800 ring-emerald-500 fill-cyan-900",
         @kind == :error && "bg-rose-50 text-rose-900 shadow-md ring-rose-500 fill-rose-900"
       ]}
       {@rest}
     >
-      <p :if={@title} class="flex items-center gap-1.5 text-sm font-semibold leading-6">
-        <.icon :if={@kind == :info} name="hero-information-circle-mini" class="h-4 w-4" />
-        <.icon :if={@kind == :error} name="hero-exclamation-circle-mini" class="h-4 w-4" />
-        <%= @title %>
-      </p>
-      <p class="mt-2 text-sm leading-5"><%= msg %></p>
-      <button type="button" class="group absolute top-1 right-1 p-2" aria-label="close">
-        <.icon name="hero-x-mark-solid" class="h-5 w-5 opacity-40 group-hover:opacity-70" />
-      </button>
+      <div class="flex items-center">
+        <.icon :if={@kind == :info} name="hero-information-circle-mini" class="h-6 w-6" />
+        <.icon :if={@kind == :error} name="hero-exclamation-circle-mini" class="h-6 w-6" />
+        <div class="flex-grow pl-1 text-sm">
+          <p :if={@title} class="font-semibold"><%= @title %></p>
+          <p class="text-sm"><%= msg %></p>
+        </div>
+        <button type="button" class="group pr-1" aria-label="close">
+          <.icon name="hero-x-mark-solid" class="h-5 w-5 opacity-40 group-hover:opacity-70" />
+        </button>
+      </div>
     </div>
     """
   end
@@ -145,9 +147,9 @@ defmodule GardenWeb.CoreComponents do
 
   def flash_group(assigns) do
     ~H"""
-    <div id={@id}>
-      <.flash kind={:info} title="Success!" flash={@flash} />
-      <.flash kind={:error} title="Error!" flash={@flash} />
+    <div id={@id} class="fixed bottom-0 left-0 right-0 z-50">
+      <.flash kind={:info} flash={@flash} />
+      <.flash kind={:error} flash={@flash} />
       <.flash
         id="client-error"
         kind={:error}
@@ -433,14 +435,11 @@ defmodule GardenWeb.CoreComponents do
 
   def header(assigns) do
     ~H"""
-    <header class={[@actions != [] && "flex items-center justify-between gap-6", @class]}>
+    <header class={[@actions != [] && "flex items-center justify-between", @class]}>
       <div>
-        <h1 class="text-lg font-semibold leading-8 text-zinc-800">
+        <h1 class="text-lg font-semibold text-zinc-800">
           <%= render_slot(@inner_block) %>
         </h1>
-        <p :if={@subtitle != []} class="mt-2 text-sm leading-6 text-zinc-600">
-          <%= render_slot(@subtitle) %>
-        </p>
       </div>
       <div class="flex-none"><%= render_slot(@actions) %></div>
     </header>
